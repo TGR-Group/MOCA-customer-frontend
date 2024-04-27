6d2c6e73f3c02ec40b143a349c5ee37f47a000c6
HARU-pc <4538haru@gmail.com>
2024-04-28 02:40:19 +0900

    🛩 move ダッシュボードに予約状況を移動

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,12 +1,34 @@
 <script setup>
-import StoreState from './StoreState.vue';
+    import StoreState from './StoreState.vue';
+
+    const VisitorID = "MC00001";
+
 </script>
 
 <template>
-    <div>
-        <h1>Dashboard</h1>
-        <p>MOCA is a web application for 文化祭</p>
+    <div class="IdBox">
+        ID:{{ VisitorID }}
     </div>
+
+    <div class="StoreBox">
+        <StoreState :StoreID=10001 />
+    </div>
+    <div class="StoreBox">
+        <StoreState :StoreID=20001 />
+    </div>
+
+    <div class="StoreBox">
+        <StoreState :StoreID=30001 />
+    </div>
+
+    <div class="StoreBox">
+        <StoreState :StoreID=40001 />
+    </div>
+
+    <div class="StoreBox">
+        <StoreState :StoreID=50001 />
+    </div>
+
 
 </template>
 
@@ -14,5 +36,18 @@ import StoreState from './StoreState.vue';
 .StoreBox {
     margin: 1.0em 0;
     height: fit-content;
+}
+.IdBox {
+    margin: auto auto;
+    height: fit-content;
+    width: fit-content;
+    text-align: center;
+    font-size: 1.5em;
+    font-weight: bold;
+    color: #000000;
+    background-color: #ffffff;
+    padding: 0.5em;
+    border-radius: 0.5em;
+    box-shadow: 0 0 0.5em #000000;
 }
 </style>
--- src/components/introduction.vue
+++ src/components/introduction.vue
@@ -1,4 +1,5 @@
 <script setup>
+import StoreState from './StoreState.vue';
 </script>
 
 <template>
@@ -6,26 +7,6 @@
         <h1>出し物紹介</h1>
         <p>MOCA is a web application for 文化祭</p>
     </div>
-
-    <div class="StoreBox">
-        <StoreState :StoreID=0 />
-    </div>
-    <div class="StoreBox">
-        <StoreState :StoreID=1 />
-    </div>
-
-    <div class="StoreBox">
-        <StoreState :StoreID=2 />
-    </div>
-
-    <div class="StoreBox">
-        <StoreState :StoreID=3 />
-    </div>
-
-    <div class="StoreBox">
-        <StoreState :StoreID=4 />
-    </div>
-
 </template>
 
 <style>
