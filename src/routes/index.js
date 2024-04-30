a9c1c9d8ed5a15fbb2d90620e36e624930aabe9e
HARU-pc <4538haru@gmail.com>
2024-05-01 06:38:17 +0900

    ➕ add 詳細ページ作成

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
     { path: "/introduction", component: () => import("../components/introduction.vue") },
     { path: "/", component: () => import("../components/dashboard.vue") },
+    { path: "/introduction/detail/:StoreID", component: () => import("../components/StoreDetail.vue") },
 ]
 
 const router = createRouter({
