8f86e71eb2a1ba7edc6b753674c0e39c6528e60b
HARU-pc <4538haru@gmail.com>
2024-05-01 07:51:37 +0900

    ➕ add ページトップへのボタン作成

---
+++ src/components/ToPageTop.vue
@@ -0,0 +1,28 @@
+<template>
+    <a class="pagetop" href="#"><div class="pagetop__arrow"></div></a>
+</template>
+
+<style scoped>
+    .pagetop {
+        height: 50px;
+        width: 50px;
+        position: fixed;
+        right: 30px;
+        bottom: 30px;
+        background: #a0a0a055;
+        border: solid 2px #000;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 2;
+    }
+
+    .pagetop__arrow {
+        height: 10px;
+        width: 10px;
+        border-top: 3px solid #000;
+        border-right: 3px solid #000;
+        transform: translateY(20%) rotate(-45deg);
+    }
+</style>
