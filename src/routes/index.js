68332bfd73e34d61a697e74fdcf94b1746fe58df
HARU-pc <4538haru@gmail.com>
2024-05-06 08:37:26 +0900

    ⬆️ update ページ遷移時の位置を調整

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,13 @@ const routes = [
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        }
+        return { top: 0 }
+    }
 })
 
 export default router;
