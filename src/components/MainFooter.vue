ecf914348c76781d32483424de26f487ad6b3e9a
HARU-pc <4538haru@gmail.com>
2024-05-13 13:38:22 +0900

    ⬆️ update フッターのレイアウト調整

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,41 +1,55 @@
 
 import router from '../routes';
 <template>
-    <footer>
-        <div class="FooterToHome">
-            <router-link to="/" class="FooterToHomeText" >HOME</router-link>
+    <div class="Footer">
+        <div class="FooterToHomeBg">
+            <router-link to="/" class="FooterToHome" >HOME</router-link>
         </div>
-    </footer>
+    </div>
 </template>
 
 <style scoped>
-    footer {
+    .Footer {
         position: fixed;
+        display: flex;
+        justify-content: center;
+        align-items: center;
         left: 0;
         bottom: 0;
         width: 100vw;
-        background-color: #333;
+        height: 1.5rem;
+        background-color: #cee3e3;
         color: white;
         text-align: center;
         padding: 10px;
     }
 
     .FooterToHome {
-        display: block;
-        width: 7rem;
-        height: 7rem;
+        display: flex;
+        padding: auto;
+        font-size: 1.5rem;
+        width: 5rem;
+        height: 5rem;
         border-radius: 50%;
-        color: white;
-        background-color: aqua;
+        color: rgb(34, 34, 34);
+        background-color: rgb(255, 255, 255);
         text-decoration: none;
         text-align: center;
+        align-items: center;
+        justify-content: center;
     }
-
-    .FooterToHomeText {
-        text-align: center;
-        margin: auto;
-        font-size: 2.0rem;
-        color: white;
+    .FooterToHomeBg {
+        display: flex;
+        position: fixed;
+        bottom: 0;
+        padding: 0.3rem;
+        border-radius: 50%;
+        color: rgb(34, 34, 34);
+        background-color: #cee3e3;
         text-decoration: none;
+        text-align: center;
+        align-items: center;
+        justify-content: center;
+        margin-bottom: 5px;
     }
 </style>
