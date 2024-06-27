006ac498b919f5ea0b95aa6db710d8512e729fdb
HARU-pc <4538haru@gmail.com>
2024-06-28 00:04:25 +0900

    remove console log

--- app/src/global/dbFunctions.js
+++ app/src/global/dbFunctions.js
@@ -14,7 +14,6 @@ export async function getUserData() {
             await axios.post('/register')
                 .then(response => {
                     localStorage.setItem('userData', JSON.stringify(response.data));
-                    console.log(response.data);
                     userData = response.data;
                 })
                 .catch(error => {
